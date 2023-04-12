a18fe55751d730f91a5419160115167b635d01b6
Kateryna Skepska <118077803+wonderotter@users.noreply.github.com>
2023-04-12 22:12:58 +0300

    taska1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+};
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+    refs.stopBtn.disabled = '';
+    refs.startBtn.disabled = 'disabled';
+    document.body.style.backgroundColor = getRandomHexColor();
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function onStop() {
+    refs.startBtn.disabled = '';
+    clearInterval(timerId);
+}
